98ef0c07fe91d0facc72fcd03a71bc95ccf92fd4
Martin Tomka <martintomka@microsoft.com>
2023-08-29 12:46:27 +0200

    Add new test that demonstrates how to use custom `TelemetryListener`

---
+++ test/Polly.Extensions.Tests/Issues/IssuesTests.CollectResilienceEvents.cs
@@ -0,0 +1,49 @@
+﻿using Polly.Telemetry;
+
+namespace Polly.Extensions.Tests.Issues;
+
+public partial class IssuesTests
+{
+    private static readonly ResiliencePropertyKey<List<ResilienceEvent>> ResilienceEvents = new("MyFeature.ResilienceEvents");
+
+    [Fact]
+    public void CollectResilienceEvents_UsingTelemetryListener()
+    {
+        // arrange
+        var telemetryOptions = new TelemetryOptions();
+        telemetryOptions.TelemetryListeners.Add(new ResilienceEventListener());
+
+        // prepare the resilience context
+        var context = ResilienceContextPool.Shared.Get();
+        var events = new List<ResilienceEvent>();
+        context.Properties.Set(ResilienceEvents, events);
+
+        var pipeline = new ResiliencePipelineBuilder()
+            .AddRetry(new()
+            {
+                Delay = TimeSpan.Zero,
+                ShouldHandle = _ => PredicateResult.True
+            })
+            .ConfigureTelemetry(telemetryOptions)
+            .Build();
+
+        // act
+        pipeline.Execute(_ => { }, context);
+
+        // assert
+        events.Where(e => e.EventName == "ExecutionAttempt").Should().HaveCount(4);
+        events.Where(e => e.EventName == "OnRetry").Should().HaveCount(3);
+        events.Should().HaveCount(7);
+    }
+
+    private class ResilienceEventListener : TelemetryListener
+    {
+        public override void Write<TResult, TArgs>(in TelemetryEventArguments<TResult, TArgs> args)
+        {
+            if (args.Event.Severity >= ResilienceEventSeverity.Warning && args.Context.Properties.TryGetValue(ResilienceEvents, out var events))
+            {
+                events.Add(args.Event);
+            }
+        }
+    }
+}
